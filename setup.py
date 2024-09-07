5c630bd1b50d94ec499aac96ad7d20504300361d
Harshit <develop@enkompass.net>
2024-09-08 03:38:23 +0530

    Upgraded django-url-shortening to be compatible with Django 5.0

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ def read(fname):
 
 
 setup(
-    name="django-url-shortening",
+    name="django-urlshortening",
     version="1.0.0",
-    url='https://github.com/doubletapp/django-url-shortening',
+    url='https://github.com/Enkompass/django-urlshortening',
     license='BSD',
     description="A URL shortening app for Django.",
     long_description=read('README.rst'),
 
-    author='Doubletapp',
-    author_email='info@doubletapp.ru',
+    author='Enkompass',
+    author_email='jvazquez@enkompass.net',
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
 
-    install_requires=['setuptools', 'six'],
+    install_requires=['setuptools', 'six', 'Django>=3.2,<6.0'],
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -28,7 +28,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Internet :: WWW/HTTP',
     ]
 )
--- src/urlshortening/admin.py
+++ src/urlshortening/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from urlshortening.models import Url
 
 
+@admin.register(Url)
 class UrlsAdmin(admin.ModelAdmin):
     list_display = ('short_id', 'url', 'pub_date', 'redirect_count')
     ordering = ('-pub_date',)
 
-admin.site.register(Url, UrlsAdmin)
